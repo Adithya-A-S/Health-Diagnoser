bd98c11b2f4ecb5432cf6abbdaf73ac604060fc9
Adithya-A-S <hariadithya78@gmail.com>
2022-07-06 03:45:08 +0530

    master

--- health-app/src/App.js
+++ health-app/src/App.js
@@ -5,8 +5,10 @@ import Diabetes from  './components/Diabetes/Diabetes';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Navbar/Nav'
 import Home from './components/Home/Home'
-//import Login from './components/Login/Login'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/Login/Login'
 import {BrowserRouter as Router, Switch,Route, Routes}from 'react-router-dom';
+
 
 
 export default function App() {
@@ -18,6 +20,7 @@ export default function App() {
         <Routes>
           <Route path = "/home" element={<Home/>}/>
           <Route exact path = "/diabetes" element={<Diabetes/>}/>
+          <Route exact path = "/login" element={<Login/>}/>
         </Routes>
         <Footer/>
       </div>
